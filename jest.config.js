a9f48fca13d753ebf2d5af9573e7346003d404e4
Nicklc7 <elazaroc@uni.pe>
2023-02-07 01:10:32 -0500

    chore: add alias path config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    'test/(.*)': '<rootDir>/test/$1'
+    'test/(.*)': '<rootDir>/test/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@user/(.*)$': '<rootDir>/src/modules/user/$1',
+    '^@db/(.*)$': '<rootDir>/src/db/$1'
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,ts}']
